937f3e10697b7e7415a0ca249b7df6ce748a3f36
Obai Yavana <oyava4825@gmail.com>
2021-06-24 15:30:33 -0400

    All MVP goals except style using flexbot, and deploy site are completed

--- app.js
+++ app.js
@@ -6,52 +6,64 @@
 
 
 // Global variables:
-// Creates a variable called statesDiv that refers to the state class.
-// const stateDiv = document.querySelector(".states");
-// Creates a variable called button that refers to the button class.
-// const button = document.querySelector(".button");
-// ...
-// const dataContainer = document.querySelector("state-#data");
+const dataContainer = document.querySelector("#city-data");
 
 
 
 
-const form = document.querySelector('form')
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const cityTextInput = document.querySelector('#city-search').value
-  console.log(cityTextInput)
-  const stateOptionInput = document.querySelector('#select-state').value
-  console.log(stateOptionInput)
-  fetchData(cityTextInput, stateOptionInput)
-})
-// Try/catch & async/await function for each state (50 total).
+// Pulls API data and logs errors.
 async function fetchData(city, state) {
   try {
-    // const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&units=imperial&appid=1d015797722f985c58d03693976c4b24`
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=1d015797722f985c58d03693976c4b24&units=imperial`
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=1d015797722f985c58d03693976c4b24&units=imperial`;
     const response = await axios(url);
-    console.log(response.data);
-    showStateData(response.data);
+    // console.log(response.data);
+    // Removes the previous data container before creating a new one.
+    removeElement(dataContainer);
+    showCityData(response.data);
     return response;
   } catch (error) {
     console.error(error);
   }
 };
-// Create dynamic HTML.
-function showStateData(data) {
-  const dataContainer = document.querySelector("#state-data");
-  console.log(data);
-  let stateData = `<h1>${data.name}</h1> <h3>Temperature: ${data.main.temp}</h3> <h3>Weather description: ${data.weather[0].description}</h3> <h3>Timezone: ${data.timezone}</h3>`
-  dataContainer.insertAdjacentHTML("beforeend", stateData);
-  return stateData;
+
+
+
+
+// Creates a variable that targets the form element.
+const form = document.querySelector('form');
+
+// When the submit button is clicked...
+form.addEventListener('submit', (e) => {
+  // Prevents page from reloading when submit is clicked.
+  e.preventDefault();
+  // Takes value from text input box.
+  const cityTextInput = document.querySelector('#city-search').value;
+  // console.log(cityTextInput);
+  // Takes value from from dropdown menu.
+  const stateOptionInput = document.querySelector('#select-state').value;
+  // console.log(stateOptionInput);
+  // Pulls data from API using the values from cityTextInput and stateOptionInput as parameters.
+  fetchData(cityTextInput, stateOptionInput);
+});
+
+
+
+
+// Creates a data container to store data pulled from step above (Dynamic HTML).
+function showCityData(data) {
+  const dataContainer = document.querySelector("#city-data");
+  // console.log(data);
+  let cityData = `<h1>${data.name}</h1> <h3>Temperature: ${data.main.temp}</h3> <h3>Weather description: ${data.weather[0].description}</h3> <h3>Timezone: ${data.timezone}</h3>`;
+  dataContainer.insertAdjacentHTML("beforeend", cityData)
+  return cityData;
 };
-// Remove previous state data.
-function removeState(elementToRemove) {
-  while (elementToRemove.lastChild) {
-    elementToRemove.removeChild(elementToRemove.lastChild);
+
+
+
+
+// Removes previous city data.
+function removeElement(element) {
+  while (element.lastChild) {
+    element.removeChild(element.lastChild);
   }
 };
-// Global variables for dropdown menu.
-const selectTag = document.querySelector("#select-state");
-const dataContainer2 = document.querySelector("#data-container");
